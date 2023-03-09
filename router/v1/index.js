45575e6d2b3794de5504f3bbb7a256b5cc6fc61d
tulika03 <tulika@thelattice.in>
2023-03-09 17:38:09 +0530

    block singular post and put operation

--- router/v1/index.js
+++ router/v1/index.js
@@ -1,7 +1,7 @@
 let router = require('express').Router();
 
 router.use("/sync", require("./patientBundle"));
-//router.use("/", require("./resource"));
+router.use("/", require("./resource"));
 
 
 module.exports = router;
