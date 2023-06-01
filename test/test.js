82dab9ecfd6d9cbf4b0903f2daa18d44543cca40
tulika03 <tulika@thelattice.in>
2023-06-01 17:20:13 +0530

    changes in schema validation as some validation described didn't work properly

--- test/test.js
+++ test/test.js
@@ -5,17 +5,22 @@ const app = require("../app")
 let request = require('supertest')(app);
 const { v4: uuidv4 } = require('uuid');
 let token = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsInVzZXJOYW1lIjoiVHVsaWthIiwiaWF0IjoxNjg1NDE5ODMxLCJleHAiOjE2ODU4NTE4MzF9.YDfW3NAkod3mZ0kIEZ6gYoS04jx4C3_SGk5RvMTlz3Y`;
-let jsonMedication = [{
-    "medFhirId": "string",
-    "medCode": "string",
-    "medName": "string",
-    "doseForm": "string",
-    "doseFormCode": "string",
-    "activeIngredient": "string",
-    "activeIngredientCode": "string",
-    "medUnit": "string",
-    "medNumeratorVal": "number"
-}];
+let jsonMedication = {
+    "title": "Medication data schema",
+    "type": "object",
+    "properties": {
+        "medFhirId": {type: "string"},
+        "medCode":  {type: "string"},
+        "medName":  {type: "string"},
+        "doseForm":  {type: "string"},
+        "doseFormCode":  {type: "string"},
+        "activeIngredient":  {type: "string"},
+        "activeIngredientCode":  {type: "string"},
+        "medUnit":  {type: "string"},
+        "medNumeratorVal":  {type: "number"}
+    }
+
+};
 let medicationList = [];
 
 describe('GET /api/v1/Medication', () => {
@@ -26,7 +31,7 @@ describe('GET /api/v1/Medication', () => {
         expect(response.body.status).to.eql(2);
         expect(response.body.message).to.eql("details fetched successfully");
         expect(response.body.data).to.be.an('array').that.is.not.empty;
-        response.body.data.forEach(medicine => expect(medicine).to.be.jsonSchema(jsonMedication));
+        response.body.data.forEach(medicine => {console.info(medicine); return expect(chai.tv4.validate(medicine, jsonMedication)).to.be.true });
         medicationList = response.body.data;
     });
 
@@ -40,14 +45,19 @@ describe('GET /api/v1/sct/medTime', () => {
         expect(response.body.status).to.eql(1);
         expect(response.body.message).to.eql("Data fetched successfully");
         expect(response.body.data).to.be.an('array').that.is.not.empty;
-        let schemaTiming = [{
-            "medinstructionCode": "string",
-            "medinstructionVal": "string"
-        }];
+        let schemaTiming = {
+            "title": "Schema timing",
+            "type": "object",
+            "properties" : {
+                "medinstructionCode":  {type: "string"},
+                "medinstructionVal": {type: "string"}
+            }
+
+        };
 
         let timingList = ["Before meal", "Before food", "An hour before food or on an empty stomach", "Half to one hour before food", "Before lunch", "Between meals", "With snack", "During meal", "During feed", "Postprandial", "After feed", "After food", "With or after meal", "After dinner", "After lunch", "Daily with breakfast", "Daily with dinner", "Daily with lunch", "With breakfast", "With dinner", "With largest meal", "With lunch", "With meals", "With or after food", "With supper", "Without regard to meals"];
         let resTimingList = response.body.data.map(e => e.medinstructionVal);
-        response.body.data.forEach(instruction => expect(instruction).to.be.jsonSchema(schemaTiming));
+        response.body.data.forEach(instruction => expect(chai.tv4.validate(instruction, schemaTiming)).to.be.true);
         expect(timingList).to.have.members(resTimingList);
     });
 });
@@ -110,29 +120,43 @@ let inputPrescriptionData =
 ]
 
 let prescriptionSchemaFormat = {
-    "patientId": "string",
-    "generatedOn": {
-        "type": "string",
-        "format": "date-time"
-    },
-    "prescriptionId": "string",
-    "prescription": "array"
-}
-let medReqSchema = {
-    "medFhirId": "string",
-    "qtyPerDose": "number",
-    "doseForm": "string",
-    "doseFormCode": "string",
-    "frequency": "number",
-    "timing": {
-        "type": ["string", "null"]
-    },
-    "duration": "number",
-    "qtyPrescribed": "number",
-    "note": {
-        "type": ["string", "null"]
+    title: "prescription schema",
+    "type": "object",
+    "properties": {
+        "patientId": "string",
+        "generatedOn": {
+            "type": "string",
+            "format": "date-time"
+        },
+        "prescriptionId": "string",
+        "prescription": {
+            "type": "array",
+            "minItems": 1,
+            "items": [{
+                type: 'object',
+                properties: {
+                    "medFhirId":  {type: "string"},
+                    "qtyPerDose":  {type: "number"},
+                    "doseForm":  {type: "string"},
+                    "doseFormCode":  {type: "string"},
+                    "frequency":  {type: "number"},
+                    "timing": {
+                        "type": ["string", "null"],
+                    },
+                    "duration":  {type: "number"},
+                    "qtyPrescribed":  {type: "number"},
+                    "note": {
+                        "type": ["string", "null"]
+                    }
+                }
+            }]
+        }
     }
+
 };
+
+
+
 let prescription2 = 
 [{
     "patientId": "20154",
@@ -205,11 +229,7 @@ let prescription2 =
 ]
 describe('POST /api/v1/sync/MedicationRequest', () => {
     for (let prescription of inputPrescriptionData) {
-        expect(prescription).to.be.jsonSchema(prescriptionSchemaFormat);
-        for (let med of prescription.prescription) {
-            expect(med.medFhirId)
-            expect(med).to.be.jsonSchema(medReqSchema);
-        }
+        expect(chai.tv4.validate(prescription, prescriptionSchemaFormat)).to.be.true;
     }
     it('Save prescription data for multiple patients and their visits', async () => {
         const response = await request.post('/api/v1/sync/MedicationRequest').set({'x-access-token': token})
@@ -246,39 +266,27 @@ describe('GET /api/v1/MedicationRequest', () => {
         expect(response.body.data).to.be.an('array').that.is.not.empty;
         let result = response.body.data;
         for (let prescription of result) {
-            expect(prescription).to.be.jsonSchema(prescriptionSchemaFormat);
-            for (let med of prescription.prescription) {
-                expect(med.medFhirId)
-                expect(med).to.be.jsonSchema(medReqSchema);
-            }
+            expect(chai.tv4.validate(prescription, prescriptionSchemaFormat)).to.be.true;
         }
     });
-    it('calls list of prescriptions of patient id: 21028', async () => {
-        const response = await request.get('/api/v1/MedicationRequest?patientId=21028').set({ 'x-access-token': token });
+    it('calls list of prescriptions of all patients', async () => {
+        const response = await request.get('/api/v1/MedicationRequest?_count=500').set({ 'x-access-token': token });
         expect(response.status).to.eql(200);
         expect(response.body).to.have.property("status");
         expect(response.body.status).to.eql(1);
         expect(response.body.message).to.eql("details fetched successfully");
         prescriptionSchemaFormat.prescriptionFhirId = "string";
         expect(response.body.data).to.be.an('array').that.is.not.empty;
-        response.body.data.forEach(medicine => expect(medicine).to.be.jsonSchema(jsonMedication));
+        response.body.data.forEach(medicine =>  expect(chai.tv4.validate(medicine, jsonMedication)).to.be.true);
         let result = response.body.data;
         for (let prescription of result) {
-            expect(prescription).to.be.jsonSchema(prescriptionSchemaFormat);
-            for (let med of prescription.prescription) {
-                expect(med.medFhirId)
-                expect(med).to.be.jsonSchema(medReqSchema);
-            }
+            expect(chai.tv4.validate(prescription, prescriptionSchemaFormat)).to.be.true;
         }
     }); 
     
     describe('POST /api/v1/sync/MedicationRequest', () => {
         for (let prescription of prescription2) {
-            expect(prescription).to.be.jsonSchema(prescriptionSchemaFormat);
-            for (let med of prescription.prescription) {
-                expect(med.medFhirId)
-                expect(med).to.be.jsonSchema(medReqSchema);
-            }
+            expect(chai.tv4.validate(prescription, prescriptionSchemaFormat)).to.be.true;
         }
         it('Save prescription data for patient 20154 and her visits', async () => {
             const response = await request.post('/api/v1/sync/MedicationRequest').set({'x-access-token': token})
