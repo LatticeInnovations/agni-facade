20f27ae99e125710a1c43944d33e7fcb07dad0f7
tulika03 <tulika@thelattice.in>
2023-11-02 11:47:35 +0530

    patient id enhacement https://github.com/LatticeInnovations/vanuatu-mdr-main/issues/21#issuecomment-1790085137

--- controllers/authController.js
+++ controllers/authController.js
@@ -51,7 +51,7 @@ const login = async function (req, res) {
             response = { status: 0, "message": message }            
         }
         else {
-            const sessionData = setSessionId(authData.sessionCount, authData.user_id);
+            const sessionData = setSessionId();
             loginAttempts = 0;
             otpCheckAttempt = 0;
             otpGenAttempt = 0;
@@ -213,7 +213,7 @@ let verifyOTP = async function (req, res) {
                 upsertJson = { "login_attempts": loginAttempts, "attempt_timestamp": currentTime, "otp_check_attempts": otpCheckAttempt, "otp_generate_attempt": otpGenAttempt, "otp": null, "otp_gen_time": null, "otpVerified": true};
                 const userInfo = setUserDetail(userDetail.profile);
                 if(req.body.isMobile) {
-                    const sessionData = setSessionId(authData.sessionCount, userDetail.profile.user_id);
+                    const sessionData = setSessionId();
                     userInfo.sessionId = sessionData.sessionId;
                     upsertJson.sessionCount = sessionData.counterVal;
                     upsertJson.sessionId = sessionData.sessionId ;
@@ -339,23 +339,23 @@ const changePassword = async function (req, res) {
 
 // function to create a new session id for every login
 
-function setSessionId (sessionCount, userId) {
+function setSessionId () {
     try {
         let incrementMonth = sessionCounter.monthCounter;
         if(sessionCounter.montIncDate == null || new Date(sessionCounter.montIncDate).getMonth() != new Date().getMonth()) {
             incrementMonth = sessionCounter.monthCounter + 1;
             sessionCounter.monthCounter = incrementMonth;
             sessionCounter.montIncDate = new Date();
+            sessionCounter.sessionIdInc = 0;
         }
-        let sessionIncVal = sessionCount + 1;
+        let sessionIncVal = sessionCounter.sessionIdInc + 1;
         let monthIncPadded = String(incrementMonth).padStart(2, "0");
-        let sessionIncPadded = String(sessionIncVal).padStart(2, "0");
-        let userIdPadded = String(userId).padStart(3, "0");
+        let sessionIncPadded = String(sessionIncVal).padStart(3, "0");
         sessionCounter.sessionIdInc = sessionIncVal;
         let fileName = __dirname + "/../utils/sessionCounter.json";
         let counterJson=sessionCounter;
         writeToFile(fileName, counterJson);
-        const sessionId =  monthIncPadded + "-" + sessionIncPadded + "-" + userIdPadded;
+        const sessionId =  monthIncPadded + "-" + sessionIncPadded;
         console.info("sessionCounter: ", sessionCounter)
         return {
             sessionId: sessionId, incrementVal: incrementMonth, counterVal: sessionIncVal
