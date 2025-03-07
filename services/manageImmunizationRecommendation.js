557290eba117ee5ec310e4efaada47ecfbb3d5bc
tulika03 <tulika@thelattice.in>
2025-03-07 12:07:19 +0530

    https://github.com/LatticeInnovations/LatticeOnFhirMain/issues/955

--- services/manageImmunizationRecommendation.js
+++ services/manageImmunizationRecommendation.js
@@ -1,9 +1,4 @@
-let bundleOp = require("./bundleOperation");
-let bundleFun = require("./bundleOperation");
-let config = require("../config/nodeConfig");
-const { v4: uuidv4 } = require('uuid');
 let ImmunizationRecommendation = require('../class/ImmunizationRecommendation');
-const vaccines = require('../utils/vaccines.json');
 
 let setImmunizationRecommendationData = async function (resType, reqInput, FHIRData, reqMethod, reqQuery, token) {
     try {
